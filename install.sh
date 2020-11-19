3a72f87d7a7da34cb8bcda5c57299da0562db5c9
Mike Hourahine <mike@hourahine.net>
2020-11-19 10:15:38 -0500

    Including powerlevel10k in config

--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm ~/.zshrc
 
-# install powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
+# install powerlevel9
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
