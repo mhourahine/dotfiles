03ca5fa804b095dd9c2c6be7cbe2ab36ac6581de
Mike Hourahine <mike@hourahine.net>
2023-04-21 17:27:01 -0400

    Staring neovim config

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,42 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Buffer nav
+keymap("n", "<leader>g", ":b#<cr>", opts)
+keymap("n", "<leader>d", ":Lex 30<cr>", opts)
+
+-- Selecting
+keymap("n", "<leader>y", ":w !pbcopy<cr><cr>", opts)
+keymap("n", "<leader>a", "ggVG", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
