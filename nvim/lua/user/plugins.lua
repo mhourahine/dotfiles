67ab15f3cb9ead40b308829c7f4c1e2d4064f02a
Mike Hourahine <mike@hourahine.net>
2023-05-10 08:30:13 -0400

    Adding tokyonight theme to neovim, alacritty and tmux

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -94,6 +94,9 @@ return packer.startup(function(use)
 		requires = "nvim-lua/plenary.nvim",
 	})
 
+	-- Theme
+	use 'folke/tokyonight.nvim'
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -203,6 +203,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/mike/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["tokyonight.nvim"] = {
+    loaded = true,
+    path = "/Users/mike/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    url = "https://github.com/folke/tokyonight.nvim"
   }
 }
 
